3d80bde71e22900f5664bc6e1df94f2a7cb97743
Jackson Hammond <jbhammon@users.noreply.github.com>
2021-02-12 17:31:36 -0500

    Modal now appears

--- src/views/BudgetDetail.vue
+++ src/views/BudgetDetail.vue
@@ -2,18 +2,73 @@
     <div>
         <h2 v-if='this.budget'>Budget > {{this.budget.title}} </h2>
         <h2 v-else>Budget</h2>
+
+        <button
+            type="button"
+            class="btn"
+            @click="showModal"
+            >
+            Add Category
+        </button>
+
+        <modal
+            v-show="isModalVisible"
+            @close="closeModal"    
+        >
+            <template v-slot:header>
+                Add a category
+                <button
+                    type="button"
+                    class="btn-close"
+                    @click="closeModal"
+                    aria-label='Close modal'
+                >
+                    x
+                </button>
+            </template>
+            <template v-slot:body>
+                <label for='item-category'>Category</label>
+                <select id='item-category' name='category' v-model='nextCategory'>
+                    <option v-bind:key=id v-for="(name, id) in categories" v-bind:value="id">
+                        {{ name }}
+                    </option>
+                </select>
+                
+                <label for='item-amount'>Amount</label>
+                <input id='item-amount' type='text' v-model="nextAmount">
+            </template>
+            <template v-slot:footer>
+                <button @click="saveIncome(true)">
+                    Save and Close
+                </button>
+                <button @click="saveIncome(false)">
+                    Save and add another
+                </button>
+            </template>
+        </modal>
     </div>
 </template>
 
 <script>
 import axios from 'axios';
 
+import modal from '../components/Modal';
+
 export default {
     name: "BudgetDetail",
-    components: {},
-    methods: {},
-    create() {
-      axios.get(`http://localhost:8000/budgets/${this.$router.query.id}`)
+    components: {
+        modal,
+    },
+    methods: {
+        showModal() {
+            this.isModalVisible = true;
+        },
+        closeModal() {
+            this.isModalVisible = false;
+        },
+    },
+    mounted() {
+      axios.get(`http://localhost:8000/budgets/${this.$route.params.id}`)
       .then(res => {
         this.budget = res.data;
       });
@@ -36,6 +91,9 @@ export default {
     data () {
         return {
           budget: null,
+          nextCategory: '',
+          nextAmount: '',
+          isModalVisible: false,
         }
     },
 }
