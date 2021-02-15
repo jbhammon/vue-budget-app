0b1be15bd5fc1b3911c6f4e8d42c0f57bbc81fb0
Jackson Hammond <jbhammon@users.noreply.github.com>
2021-02-14 21:21:10 -0500

    Budgets can do basic aggregating of spending

--- finances/budgeting/models.py
+++ finances/budgeting/models.py
@@ -28,6 +28,13 @@ class Expense(models.Model):
   date = models.DateField()
   description = models.TextField(default='', blank=True, null=True)
 
+  def save(self, *args, **kwargs):
+    # Need to think about how to update the aggregated spending amounts
+    # on the BudgetItems when we create, update, or delete Expense records
+
+    # Call the parent save() method to save in the DB
+    super().save(*args, **kwargs) 
+
   def __str__(self):
     return str(self.amount) + ', ' + self.category.name + ', ' + str(self.date)
 
@@ -39,9 +46,28 @@ class Budget(models.Model):
 
 class BudgetItem(models.Model):
   amount = models.IntegerField(default=0)
+  spent = models.DecimalField(max_digits=8, decimal_places=2, default=0)
   category = models.ForeignKey(Category, on_delete=models.CASCADE)
   notes = models.TextField(default='', blank=True, null=True)
   parent_budget = models.ForeignKey(Budget, on_delete=models.CASCADE)
+
+  def save(self, *args, **kwargs):
+    # calculate actual spending for this item
+    start_date = self.parent_budget.start_date
+    end_date = self.parent_budget.end_date
+    aggregate_dict = Expense.objects.filter(
+      category=self.category
+    ).filter(
+      date__gte=start_date
+    ).filter(
+      date__lte=end_date
+    ).aggregate(
+      models.Sum('amount')
+    )
+    self.spent = aggregate_dict['amount__sum']
+
+    # Call the parent save() method to save in the DB
+    super().save(*args, **kwargs)
 
   def __str__(self):
     return str(self.amount) + ', ' + self.category.name + ', ' + self.parent_budget.title
